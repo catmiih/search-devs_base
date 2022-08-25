ea44de1298ffb5914d81d695d8c86865ca8bad33
catmiih <emilyleme.dev@gmail.com>
2022-08-24 21:53:41 -0300

    1.0.0.3

--- back/filter/filter_func.php
+++ back/filter/filter_func.php
@@ -5,52 +5,69 @@
     Developed by SearchDevs™ for SearchDevs™ Plataform.
 
     All rights reserved ©️. All materials are protected by copyright and other rights.
-    Version : 1.0.0.2 - Aug, 2022.
+    Version : 1.0.0.3 - Aug, 2022.
 */
 
-function filterFunction($approved, $list_approve, $type, $type_user, $skills, $user_skills, $proj_level, $skill_level) {
-
-    /* Total Skills with Match */
-    $total_skills = 0;
-
-    /* START FILTER */
+function approved($approved, $list_approve, $type, $type_user, $skills, $user_skills, $proj_level, $skill_level)
+{
     if ($approved <= $list_approve) {
-        if ($type == $type_user) {
-
-        /* Difference of Skills and User_Skills */
-            $diff = count(array_diff($skills,$user_skills));
-        /* ---------------------- */
-
-            if ($diff == count($skills)) {
-                startFilter($diff, $skills, $proj_level, $skill_level, $total_skills, $approved);
-            }else
-            if ($diff >= (count($skills)/100) * 20) { /* ⬅ Skill difference + Error Margin */
-                startFilter($diff, $skills, $proj_level, $skill_level, $total_skills, $approved);
-            }else
-                exit;
-        } else {
-            /* Professional is not good for project. */
-            return false;
+        $result = filterFunction($type, $type_user, $skills, $user_skills, $proj_level, $skill_level);
+        if ($result) {
+            return $approved+=1;
         }
     }
 }
 
-function startFilter($skills, $proj_level, $skill_level, $total_skills, $approved) {
+function filterFunction($type, $type_user, $skills, $user_skills, $proj_level, $skill_level)
+{
 
-    /* Loop for scan all skills Level*/
-    for($y = 0; $y <= count($skills); $y++ ) {
-        if ($proj_level >= ($skill_level[$y]))
-            $total_skills++;
-        }
+    /* Start Filter */
+
+
+    $user_type = count(array_diff($type, $type_user));
     /* ---------------------- */
 
-    if ($total_skills >= (count($skills) - ((count($skills)/100) * 20))){
-            /* Here is the developers wen all approved for this project. */
+    if ($user_type <= (count($type) / 100) * 20) {
+
+        /* Difference in count of Skills and User_Skills */
+        $diff = count(array_diff($skills, $user_skills));
+        /* ---------------------- */
+
+        if ($diff <= (count($skills) / 100) * 20) { /* ⬅ Skill difference + Error Margin */
+            $result = startFilter($skills, $proj_level, $skill_level);
+            return $result;
+        } else
+            return false;
+    } else {
+        /* Professional is not good for project. */
+        return false;
+    }
+}
+
+function startFilter($skills, $proj_level, $skill_level)
+{
+
+    /* Total Skills with Match */
+    global $total_skills;
+
+
+    /* Loop for scan all skills Level*/
+    for ($y = 0; $y <= count($skills) - 1; $y++) {
+        if ($proj_level <= ($skill_level[$y])) {
+            $total_skills++;
+        } else {
+        }
+    }
+
+    /* ---------------------- */
+
+    if ($total_skills >= (count($skills) - ((count($skills) / 100) * 20))) {
+        /* Here is the developers wen all approved for this project. */
         if ($total_skills == count($skills)) {
             /* If has a perfect professional: */
-            return $approved++ && $list_approve=5;    
-        } else
-            return $approved++;
+            return $list_approve = 5;
+        }
+    } else {
     }
 }
 
--- back/filter/project_add.php
+++ back/filter/project_add.php
@@ -5,15 +5,15 @@
     Developed by SearchDevs™ for SearchDevs™ Plataform.
 
     All rights reserved ©️. All materials are protected by copyright and other rights.
-    Version : 1.0.0.2 - Aug, 2022.
+    Version : 1.0.0.3 - Aug, 2022.
 */
 
 /* Pick information of the company/project. */
-$projId = "";
-$projName = "";
-$type = []; /* ⬅ Occupation area */
-$skills = [];
-$proj_level;
+$projId = 10;
+$projName = "IBM-WEB";
+$type = ['Web']; /* ⬅ Occupation area */
+$skills = ['HTML5','CSS3','JAVA'];
+$proj_level=1;
 
 /* Specific Config */
 $dateStart;
@@ -21,13 +21,22 @@ $dateEnd;
 $payment;
 
 /* Pick informations from developers  */
-$devID = "";
-$type_user = [];
-$user_skills = [];
-$skill_level = [];
+$devID = "10";
+$type_user = ['Web'];
+$user_skills = ['HTML5','JAVA','CSS3'];
+$skill_level = ['5','4','5'];
 
 /* Filter config */
 $list_approve = 20;
+global $approved;
 
 require_once ('filter_func.php');
+
+for ($i = 0; $i < 3; $i++) {
+    $response = approved($approved, $list_approve, $type, $type_user, $skills, $user_skills, $proj_level, $skill_level);
+}
+
+
+echo '<script>alert("aprovados: ',$approved,'")</script>';
+
 ?>
