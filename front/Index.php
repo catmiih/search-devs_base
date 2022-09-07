46bdda66949811cc2d4c1fe834ce4e60ab015a8b
AnakarineBC <anakarinecadoso@gmail.com>
2022-09-07 19:17:41 -0300

    modified:   front/Index.php 	modified:   front/css/css.css     modified:   front/css/nav.css

--- front/Index.php
+++ front/Index.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" type="text/css" href="css/css.css" />
     <link rel="stylesheet" type="text/css" href="css/nav.css" />
 </head>
-
 <body>
   <div class="jumbvideo jumbvideo-fluid align-middle">
     <video autoplay="" muted="" loop="" id="banner">
@@ -20,9 +19,9 @@
     </video>
   </div>
   <nav class="navbar">
-    <a class="navbar-brand" href="Index.php" style="margin-right: 0px; padding-left: 10px;">
-        <img src="Imagens/logo.png" width="30" height="40">
-        <div id="Text">Search Devs™</div>
+    <a class="navbar-brand" href="Index.php" style="margin-right: 0px; padding-left: 25px;">
+        <img src="Imagens/logo.png" width="30" height="45">
+        <div id="Text">SEARCH DEVS™</div>
     </a>
     <ul class="nav justify-content-end">
     <li class="nav-item">
@@ -32,10 +31,28 @@
         <a class="nav-link" href="#">Login</a> 
     </li>
   </nav> 
-  <div class="Principal">
+  <div class="main">
       <h1>Search Devs</h1>
-      <p>O futuro chegou.</p>
+      <p>Um novo jeito de contratar.</p>
       <button id="Btn"><a href="TipoCad.php">Começar agora</a></button>
+  </div>
+  <div class="sub1">
+      <div class="card mb-3 bg-dark bg-gradiente text-white" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="Imagens/logo.png" class="img-fluid rounded-start" alt="Logo Search Devs">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Oque é?</h5>
+              <p class="card-text">O Search Devs é uma plataforma com o foco em auxiliar desenvolvedores autônomos a encontrarem novos projetos, com uma ferramenta inovadora que une
+              o desenvolvedor ao projeto que mais tenha a sua cara!
+              Tudo isso de forma automatizada.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </body>
 </html>
