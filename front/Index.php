63896bea0f8fe1ee7a703ce8a4da221dcdc8d7b8
AnakarineBC <anakarinecadoso@gmail.com>
2022-09-07 18:20:02 -0300

    modified:   front/Index.php 	modified:   front/css/css.css

--- front/Index.php
+++ front/Index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/8381/8381909.png" />
+    <link rel="shortcut icon" type="image/png" href="Imagens/logo.png"/>
     <link rel="stylesheet" type="text/css" href="../assets/extend/css/bootstrap.css" />
     <script src="../assets/extend/js/bootstrap.js"></script>
     <link rel="stylesheet" type="text/css" href="css/css.css" />
@@ -14,7 +14,11 @@
 </head>
 
 <body>
-
+  <div class="jumbvideo jumbvideo-fluid align-middle">
+    <video autoplay="" muted="" loop="" id="banner">
+      <source src="Imagens/back_image.mp4" type="video/mp4" />
+    </video>
+  </div>
   <nav class="navbar">
     <a class="navbar-brand" href="Index.php" style="margin-right: 0px; padding-left: 10px;">
         <img src="Imagens/logo.png" width="30" height="40">
@@ -29,13 +33,9 @@
     </li>
   </nav> 
   <div class="Principal">
-      <div class="jumbvideo jumbvideo-fluid align-middle">
-        <video autoplay="" muted="" loop="" id="banner">
-          <source src="Imagens/back_image.mp4" type="video/mp4" />
-        </video>
-      </div>
-
-      <h1></h1>
+      <h1>Search Devs</h1>
+      <p>O futuro chegou.</p>
+      <button id="Btn"><a href="TipoCad.php">Começar agora</a></button>
   </div>
 </body>
 </html>
