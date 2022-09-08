3e56220b0b7ac2b4a4489dd54ea07d4534d877ee
catmiih <emilyleme.dev@gmail.com>
2022-09-08 15:26:19 -0300

    1.0.1.5

--- back/class/user.php
+++ back/class/user.php
@@ -26,6 +26,8 @@ class User {
 
 
 /* Getter and Setter */
+
+    /* Get DEV ID */
     public function getId() {
         return $this->Dev_ID;
     }
@@ -33,23 +35,81 @@ class User {
         $this->Dev_ID = $id;
     }
 
+    /* Get DEV Name */
     public function getName() {
         return $this->Dev_name;
     }
     public function setName($name) {
-        $this->Dev_name = $iid;
+        $this->Dev_name = $name;
     }
 
+    /* Get DEV Last Name */
     public function getLastN() {
         return $this->Dev_lastN;
     }
     public function setLastN($lastN) {
         $this->Dev_name = $lastN;
     }
+
+    /* Get DEV Email */
+    public function getEmail() {
+        return $this->Dev_email;
+    }
+    public function setEmail($email) {
+        $this->Dev_email = $email;
+    }
+
+    /* Get DEV Pass */
+    public function getPass() {
+        return $this->Dev_pass;
+    }
+    public function setPass($pass) {
+        $this->Dev_pass = $pass;
+    }
+
+    /* Get DEV Tel-Number */
+    public function getNum() {
+        return $this->Dev_Num;
+    }
+    public function setNum($num) {
+        $this->Dev_Num = $num;
+    }
+
+    /* Get DEV CEP */
+    public function getCEP() {
+        return $this->Dev_cep;
+    }
+    public function setCEP($cep) {
+        $this->Dev_cep = $cep;
+    }
+
+    /* Get DEV CPF */
+    public function getCPF() {
+        return $this->Dev_cpf;
+    }
+    public function setCPF($cpf) {
+        $this->Dev_cpf = $cpf;
+    }
+
+    /* Get DEV Born Date */
+    public function getBorn() {
+        return $this->Dev_born;
+    }
+    public function setBorn($born) {
+        $this->Dev_born = $born;
+    }
+
+    /* Get DEV Sex */
+    public function getSex() {
+        return $this->Dev_sex;
+    }
+    public function setSex($sex) {
+        $this->Dev_sex = $sex;
+    }
     
 
 
-    function salvar() {
+    function register() {
         try {
             $this-> conn = new Connect();
             $sql = $this->conn->prepare("insert into produto values (NULL,?,?)");
