88b4f11f27f3c7deab151857b37c3f9b643114a1
AnakarineBC <anakarinecadoso@gmail.com>
2022-09-07 23:25:58 -0300

    modified:   front/Index.php 	modified:   front/css/css.css

--- front/Index.php
+++ front/Index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,31 +30,99 @@
         <a class="nav-link" href="#">Login</a> 
     </li>
   </nav> 
-  <div class="main">
+  <div class="main" style="align-items: center; text-align: center;">
       <h1>Search Devs</h1>
       <p>Um novo jeito de contratar.</p>
       <button id="Btn"><a href="TipoCad.php">Começar agora</a></button>
   </div>
-  <div class="sub1">
+  <div class="sub1" style="align-items: center;">
     <br><br>
       <div class="card mb-3 text-white" style="max-width: 600px;" id="Card">
-        <div class="row g-0">
+        <div class="row g-0" style="text-align: start;">
           <div class="col-md-4">
             <img src="Imagens/logo.png" class="img-fluid rounded-start" style="padding-left: 20px;"alt="Logo Search Devs">
           </div>
           <div class="col-md-8">
-            <div class="card-body">
+            <div class="card-body" >
               <h5 class="card-title">Oque é?</h5>
-              <p class="card-text justify-content-end">O Search Devs é uma plataforma com o foco em auxiliar desenvolvedores autônomos a encontrarem novos projetos, com uma ferramenta inovadora que une
-              o desenvolvedor ao projeto que mais tenha a sua cara!
-              Tudo isso de forma automatizada.</p>
+              <p class="card-text" >O Search Devs é uma plataforma com o foco em auxiliar desenvolvedores autônomos a encontrarem novos projetos, com uma ferramenta inovadora que une o desenvolvedor ao projeto que mais tenha a sua cara! Tudo isso de forma automatizada.</p>
           </div>
         </div>
       </div>
     </div>
-    <br><br>
-    
-
+    <br>
+    <br>
+    <br>
+    <br>
+    <h1 style="margin-left:38vw;">Como utilizar?</h1>
+    <br>
+    <br>
+    <div class="row row-cols-1 row-cols-md-3 g-4" style="padding: 35px; height: 100%;">
+      <div class="col">
+        <div class="card h-100">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">1- Crie sua conta</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">2- Adicione suas informações</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">3- Aguarde nossa ferramenta encontrar o projeto ideal para você!</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p style="padding-left: 304px; color: white; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 20px;">Após o processo, só iniciar sua trajetória como um profissional oficial de nossa plataforma!</p>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h1 style="margin-left:36vw; color: white; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; ">Por que utilizar?</h1>
+    <br>
+    <br>
+    <div class="row g-3" style="margin-left: 33vw;">
+      <div class="col-3">
+        <button class="b" id="Btn" style="height: 58px;">Menores taxas</button>
+      </div>
+      <div class="col-1">
+        <button class="b" id="Btn" style="height: 58px;">Maior segurança</button>
+      </div>
+    </div>
+    <br>
+    <div class="row g-3" style="margin-left: 33vw;">
+      <div class="col-3">
+        <button class="b" id="Btn" style="height: 58px;">Plataforma Otimizada</button>
+      </div>
+      <div class="col-1">
+        <button class="b" id="Btn" style="height: 58px;">Filtro de contratação</button>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div style="text-align: center; color: white; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 20px;">
+      <h1>Comece a utilizar agora!</h1>
+      <p >Agora que você já nos conhece, é a hora ideal para iniciar o seu proximo projeto com o 
+      <br>Search Devs! Seja você um profissional autônomo ou uma empresa em busca dos mesmos,<br> nossa plataforma irá auxiliar ambos a se encontrarem de forma rápida e intuitiva.</p>
+      <button id="Btn" style="width: 250px;"><a href="TipoCad.php">Começar agora</a></button>
+    </div>
   </div>
+  <footer>
+    SEARCH DEVS™2022
+  </footer>
 </body>
 </html>
