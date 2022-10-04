1087513e8f7f124ff9b62a9c03c61f6316978c2d
AnaKarineBC <anakarinecadoso@gmail.com>
2022-10-04 16:04:18 -0300

    1.0.3.0

--- front/skills.php
+++ front/skills.php
@@ -24,46 +24,81 @@
                     <h1>Finalizar Cadastro</h1>
                     <h2>Bem vindo, desenvolvedor.</h2>
                     <p>Por favor, selecione sua(s) área(s) de atuação:</p>
-                    <div class="checkbox">
-                            <div class="row">
-                                <div class="col">
-                                    <input class="form-check-input" type="checkbox" name="Skills" id="Web - Front End">
-                                        <label class="form-check-label" for="Web - Front End">
-                                            Web - Front End
-                                        </label>
-                                </div>
-                                <div class="col">
-                                </div>
-                                <div class="col">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col">
-                                </div>
-                                <div class="col">
-                                </div>
-                                <div class="col">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col">
-                                </div>
-                                <div class="col">
-                                </div>
-                                <div class="col">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col">
-                                </div>
-                                <div class="col">
-                                </div>
-                                <div class="col">
-                                </div>
-                            </div>
-                            <button type="submit" class="btn submit">Continuar</button>
-                        </form>
-                    </div>
+                    <form id="check" class="row row-cols-4 ">
+                        <div class="col">
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Web - Front End">
+                            <label class="form-check-label" for="Web - Front End">
+                                Web - Front End
+                            </label>
+                        </div>
+                        <div class="col">
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Cibersegurança">
+                            <label class="form-check-label" for="Cibersegurança">                                  
+                                Cibersegurança
+                            </label>
+                        </div>
+                        <div class="col">
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Artificial Intelligence">
+                            <label class="form-check-label" for="Artificial Intelligence">
+                                Artificial Intelligence
+                            </label>
+                        </div>
+                        <div class="col">
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Desktop Development">
+                            <label class="form-check-label" for="Desktop Development">
+                                Desktop Development
+                            </label>
+                        </div>
+                        <div class="col">    
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Web - Back End">
+                            <label class="form-check-label" for="Web - Back End">
+                                Web - Back End
+                            </label>
+                        </div>
+                        <div class="col">    
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Blockchain">
+                            <label class="form-check-label" for="Blockchain">
+                                Blockchain
+                            </label>
+                        </div>
+                        <div class="col">
+                            <input class="form-check-input" type="checkbox" name="Skills" id="DevOps">
+                            <label class="form-check-label" for="DevOps">
+                                DevOps
+                            </label>
+                        </div>
+                        <div class="col">        
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Data Science">
+                            <label class="form-check-label" for="Data Science">
+                                Data Science
+                            </label>
+                        </div>
+                        <div class="col">        
+                            <input class="form-check-input" type="checkbox" name="Skills" id=" Mobile Development">
+                            <label class="form-check-label" for=" Mobile Development">
+                                Mobile Development
+                            </label>
+                        </div>
+                        <div class="col">
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Design">
+                            <label class="form-check-label" for="Design">
+                                Design 
+                            </label>
+                        </div>
+                        <div class="col">        
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Database Analytics">
+                            <label class="form-check-label" for="Database Analytics">
+                                Database Analytics
+                            </label>
+                        </div>
+                        <div class="col">        
+                            <input class="form-check-input" type="checkbox" name="Skills" id="Outros">
+                            <label class="form-check-label" for="Outros">
+                                Outros
+                            </label>
+                        </div>
+                        <button type="submit" class="btn submit">Continuar</button>
+                    </form>
                 </div>
             </center>
         </div>
