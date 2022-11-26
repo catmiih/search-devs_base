2e554ad9af8de93014b027b0d6796fbbf2520c1c
catmiih <emilyleme.dev@gmail.com>
2022-11-26 11:26:06 -0300

    1.0.8.4

--- front/company/pages/createproj.php
+++ front/company/pages/createproj.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="../css/createproj.css" />
+<link rel="stylesheet" type="text/css" href="../css/edit-profile.css" />
 <link rel="stylesheet" type="text/css" href="../css/level.css" />
 
 
@@ -16,81 +17,32 @@
                     <div class="col">
                         <div class="row input">
                             <p class="col-1 label">Nome do projeto:</p>
-                            <input type="text" class="col form-control" name="username" id="" maxlength="25" minlength="5">
+                            <input type="text" class="col form-control" name="username" placeholder="Site de vendas" id="" maxlength="25" minlength="5">
 
                         </div>
 
                         <div class="row input">
                             <p class="col-1 label"> Data de início:</p>
-                            <input type="date" class="col form-control" name="office" id="" maxlength="25" minlength="5">
+                            <input type="date" class="col form-control" name="office" id="">
                         </div>
                     </div>
 
                     <div class="col">
                         <div class="row input">
-                            <p class="col-1 label">Pagamento por hora:</p>
-                            <input type="text" class="col form-control" name="name" id="" minlength="5">
-
+                            <p class="col-1 label">Horas por dia:</p>
+                            <input type="text" class="col form-control" placeholder="2" name="name" id="" minlength="1" maxlength="2">
                         </div>
 
                         <div class="row input">
                             <p class="col-1 label"> Data final:</p>
-                            <input type="date" class="col form-control" name="cell" id="" maxlength="25" minlength="5">
+                            <input type="date" class="col form-control" name="cell" id="">
                         </div>
                     </div>
                 </div>
             </div>
         </form>
 
-
-        <div class="skills">
-            <div class="row evaluation justify-content-between">
-
-                <h2 style="font-weight:700" class="col">Habilidades:</h2>
-
-                <div class="level-options col">
-                    <p style="margin: 1%;">Nível:</p>
-                    <label>
-                        <button class="btn-select btn-level level-1 selected show" id="level-1" type="button">1</button>
-                    </label>
-                    <label>
-                        <button class="show btn-select btn-level level-2 selected" id="level-2" type="button">2</button>
-                    </label>
-
-                    <label>
-                        <button class="show btn-select btn-level level-3 selected" id="level-3" type="button">3</button>
-                    </label>
-
-                    <label>
-                        <button class="show btn-select btn-level level-4 selected" id="level-4" type="button">4</button>
-                    </label>
-                    <label>
-                        <button class="show btn-select btn-level level-5 selected" id="level-5" type="button">5</button>
-                    </label>
-                </div>
-
-                <div class="col">
-                    <a class="btn-select1 btn" href="skills.php" style="padding: 5%;">Editar skills</a>
-                </div>
-                <br>
-                <div class="show-skills">
-                    <?php
-
-                    $skills = $user->getUserSkills($id)[0];
-
-                    foreach ($skills as $skill) {
-
-                        $skillCard = "<span id='" . $skill['Skill_ID'] . "' class='btn-level level-" . $skill['Skill_level'] . " skill-tag'><input type='hidden' name='level-" . $skill['Skill_level'] . "' value='" . $skill['Skill_level'] . "'><input type='hidden' name='skill-" . $skill['Skill_level'] . "' value='" . $skill['Skill_ID'] . "'><input type='hidden' name='area' value='" . $user->getAreaSkills($skill['Skill_ID'])[0] . "'> " . $user->getNameSkills($skill['Skill_ID'])[0] . "</span>";
-
-                        echo $skillCard, "\n";
-                    }
-
-                    ?>
-                </div>
-            </div>
-        </div>
-
-        <br>
+        <hr>
 
         <div class="area">
             <form class="select-skill" action="../functions/edit-profile.php" method="post">
@@ -174,8 +126,60 @@
                     <br>
                 </div>
             </form>
-
-            <button disabled class="btn-select1 btn-edit" type="submit" name="newProject"><i class="fa-solid fa-plus"></i> &nbsp; Criar projeto</button>
         </div>
+
+        <hr>
+
+        <div class="skills">
+            <div class="row evaluation justify-content-between">
+
+                <h2 style="font-weight:700" class="col">Habilidades:</h2>
+
+                <div class="level-options col-5">
+                    <p>Nível:</p>
+                    <label>
+                        <button class="btn-select btn-level level-1 selected show" id="level-1" type="button">1</button>
+                    </label>
+                    <label>
+                        <button class="show btn-select btn-level level-2 selected" id="level-2" type="button">2</button>
+                    </label>
+
+                    <label>
+                        <button class="show btn-select btn-level level-3 selected" id="level-3" type="button">3</button>
+                    </label>
+
+                    <label>
+                        <button class="show btn-select btn-level level-4 selected" id="level-4" type="button">4</button>
+                    </label>
+                    <label>
+                        <button class="show btn-select btn-level level-5 selected" id="level-5" type="button">5</button>
+                    </label>
+                </div>
+
+                <div class="col-3">
+                    <a class="btn-select1 btn" href="skills.php" style="padding: 5%;">Editar skills</a>
+                </div>
+                <br>
+                <div class="show-skills">
+                    <?php
+
+                    $skills = $user->getUserSkills($id)[0];
+
+                    foreach ($skills as $skill) {
+
+                        $skillCard = "<span id='" . $skill['Skill_ID'] . "' class='btn-level level-" . $skill['Skill_level'] . " skill-tag'><input type='hidden' name='level-" . $skill['Skill_level'] . "' value='" . $skill['Skill_level'] . "'><input type='hidden' name='skill-" . $skill['Skill_level'] . "' value='" . $skill['Skill_ID'] . "'><input type='hidden' name='area' value='" . $user->getAreaSkills($skill['Skill_ID'])[0] . "'> " . $user->getNameSkills($skill['Skill_ID'])[0] . "</span>";
+
+                        echo $skillCard, "\n";
+                    }
+
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <button disabled class="btn-select1 btn-edit" type="submit" name="newProject"><i class="fa-solid fa-plus"></i> &nbsp; Criar projeto</button>
+
     </center>
 </div>
